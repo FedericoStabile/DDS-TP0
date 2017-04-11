7f690f4f88bc5148c67ddb170c6a13ecb9e100af
FedericoStabile <federico_stabile@hotmail.com>
2017-04-11 17:11:51 -0300

    subo el get assignments y algo del put a revisar

--- TP0/src/main/java/dominio/HTTPService.java
+++ TP0/src/main/java/dominio/HTTPService.java
@@ -16,7 +16,7 @@ public class HTTPService {
 		this.client = Client.create();
 	}
 	
-	 //get student info
+	 //GET /student
 	public static ClientResponse obtenerEstudiante(){
 		ClientResponse response = Client . create ()
 				. resource ( "http://notitas.herokuapp.com")
@@ -27,12 +27,49 @@ public class HTTPService {
 		response.bufferEntity();
 		String stringResp = response.getEntity(String.class);
 		System.out.println(stringResp);
-		return response;
-		
+		System.out.println(response.getStatus());
+		return response;	
 		
 	}
 	
+	//GET /student/assignments
+	public static ClientResponse obtenerTareasEstudiante(){
+		ClientResponse response = Client . create ()
+				. resource ( "http://notitas.herokuapp.com")
+				. path ( "student/assignments")
+				. header ( "Authorization" , "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho")
+				. accept ( MediaType . APPLICATION_JSON)
+				. get ( ClientResponse . class );
+		response.bufferEntity();
+		String stringResp = response.getEntity(String.class);
+		System.out.println(stringResp);
+		System.out.println(response.getStatus());
+		return response;	
+		
+	}
+	
+	//put student info
+	public static ClientResponse actualizarEstudiante(){
+		
+		
+		ClientResponse response = Client . create ()
+				. resource ( "http://notitas.herokuapp.com")
+				. path ( "student/assignments")
+				. header ( "Authorization" , "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho")
+				. accept ( MediaType . APPLICATION_JSON)
+				. put ( ClientResponse . class, "s");
+		response.bufferEntity();
+		String stringResp = response.getEntity(String.class);
+		System.out.println(stringResp);
+		System.out.println(response.getStatus());
+		return response;
+	}
+	
+	
 	public static void main(String[] args) {
 		obtenerEstudiante();
+		obtenerTareasEstudiante();
+//		actualizarEstudiante();
+//		obtenerEstudiante();
 	}
 }
